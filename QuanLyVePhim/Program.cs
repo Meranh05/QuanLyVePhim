3fe913ba54d18683f6141a23c0918084121a8c16
Mera <151176410+Meranh05@users.noreply.github.com>
2025-08-14 18:05:48 +0700

    Update Program.cs

--- QuanLyVePhim/Program.cs
+++ QuanLyVePhim/Program.cs
@@ -10,11 +10,8 @@ namespace QuanLyVeXemPhim
 		  {
 				Application.EnableVisualStyles();
 				Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
 				Application.Run(new Forms.FormLogin());
-=======
 				Application.Run();
->>>>>>> 2421a47d0a8ee70f6776e1836bff0008159663c9
 		  }
 	 }
 }
